ae90ed498d67d9966762a8cbb94314159438f85c
Putri Chasana <putrinorchasana@gmail.com>
2023-11-01 16:03:15 +0700

    cobatiga

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,7 +1,7 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Coba Dua");
+        System.out.println("Raffy JO J nya jahat");
 
         // Kelas
         System.out.println("2H");
