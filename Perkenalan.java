4d61ccdd830c0144cedcb0889b3793fcd038ca7a
RaffyJO <rafiteguh6@gmail.com>
2023-11-01 11:10:34 +0700

    tambah

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,7 +1,7 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("krisna");
+        System.out.println("krisna andika");
 
         // Kelas
         System.out.println("2H");
