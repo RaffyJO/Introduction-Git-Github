b5d25fa52fffd4476d0de9f8ae0b1edc521e7f2f
atslhamny <atsilahamanyph@gmail.com>
2023-11-01 22:45:01 +0700

    tambahan bahn

--- Perkenalan.java
+++ Perkenalan.java
@@ -9,7 +9,12 @@ public class Perkenalan {
         // Harapan
         System.out.println("Mau jadi ketum wri pi boonx ");
 
+        //ini jokes1
         System.out.println("Sorong Papan Tarik Papan, Buah Keranji Dalam Perahu");
         System.out.println("Suruh Ngumpulin Jobsheet paling terdepan, Suruh Jelasin Github Tidak Tau");
+        //ini jokes2
+        System.out.println("Anak IT kalo Ketawa jadi <he3><h3/>");
+        //ini jokes 3
+        System.out.println("Buat Aplikasi buat perbaiki mental health, tapi mental health sendiri yang ilang:)");
     }
 }
