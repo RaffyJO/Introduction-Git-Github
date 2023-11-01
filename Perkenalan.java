88bca7aaaaf9dabe7e1ec4e20115d65174622e62
FarrelCaesarian <49358065+Quincy17@users.noreply.github.com>
2023-11-01 21:23:56 +0700

    Perkenalan.java

--- Perkenalan.java
+++ Perkenalan.java
@@ -4,6 +4,8 @@ public class Perkenalan {
         System.out.println("Muhammad Farrel Caesarian");
         //Kelas
         System.out.println("TI-1A/Web");
+        //Harapan
+        System.out.println("Bisa jadi FULL STACK WEB DEVELOPER");
         //Jokes
         System.out.println("kuda kuda apa yang sedang bersedih, kudapati kamu bersama dengan orang lain");
     }
