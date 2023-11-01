1ac35cc0f45db871bf93bac5962d5e87223802e2
atslhamny <atsilahamanyph@gmail.com>
2023-11-01 22:29:57 +0700

    atsilah

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,12 +1,15 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Raffy Jamil Octavialdy");
+        System.out.println("Atsilah Amany Putri Harsuma");
 
         // Kelas
-        System.out.println("2H");
+        System.out.println("1F");
 
         // Harapan
-        System.out.println("Sukses");
+        System.out.println("Mau jadi ketum wri pi boonx ");
+
+        System.out.println("Sorong Papan Tarik Papan, Buah Keranji Dalam Perahu");
+        System.out.println("Suruh Ngumpulin Jobsheet paling terdepan, Suruh Jelasin Github Tidak Tau");
     }
 }
