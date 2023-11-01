d8f0821c47b5a39a88527840d80e81974ae5a2f1
luthfi27upi <galut012@gmail.com>
2023-11-01 21:31:15 +0700

    Luthfi Web

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,12 +1,15 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Raffy Jamil Octavialdy");
+        System.out.println("Luthfi Triaswangga");
 
         // Kelas
-        System.out.println("2H");
+        System.out.println("1B");
 
         // Harapan
-        System.out.println("Sukses");
+        System.out.println("Bisa Menguasai Web Development");
+
+        //Jokes
+        System.out.println("Hewan apa yang punya banyak keahlian?Kukang, bisa jadi kukang tambal ban, kukang servis motor, kukang nasi goreng, kukang sol sepatu, dan kukang lainnya.");
     }
 }
