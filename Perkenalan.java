bb5872ac6b77fd704db6d8741e7ddaf9c5a6d714
FarrelCaesarian <49358065+Quincy17@users.noreply.github.com>
2023-11-01 21:20:21 +0700

    Perkenalan.java

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,12 +1,10 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Raffy Jamil Octavialdy");
-
-        // Kelas
-        System.out.println("2H");
-
-        // Harapan
-        System.out.println("Sukses");
+        System.out.println("Muhammad Farrel Caesarian");
+        //Kelas
+        System.out.println("TI-1A/Web")
+        //Jokes
+        System.out.println("kuda kuda apa yang sedang bersedih, kudapati kamu berduaan dengan orang lain")
     }
 }
