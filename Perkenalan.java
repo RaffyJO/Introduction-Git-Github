8c4508d458427741442cc0178fe3c5e613eb7a8a
RizkyaSalsabila <rzkysalsabila@gmail.com>
2023-11-02 13:51:22 +0700

    Perkenalan Rizkya

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,12 +1,15 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Raffy Jamil Octavialdy");
+        System.out.println("Rizkya Salsabila");
 
         // Kelas
-        System.out.println("2H");
+        System.out.println("1D - Teknik Informatika");
 
         // Harapan
-        System.out.println("Sukses");
+        System.out.println("Mengasah soft skill dan hard skill, menambah ilmu dan menguasai di bidang web developer");
+
+        //Jokes
+        System.out.println("Penyanyi yang hobi mikir, Ayu Thinking dongs");
     }
 }
